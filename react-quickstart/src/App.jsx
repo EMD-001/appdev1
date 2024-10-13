0c5694791d34bd1f02e8f094c55d99500fa9a706
EMD-001 <elloisadegula.01@gmail.com>
2024-10-13 17:30:10 +0800

    I'm done completing the section on Rendering lists

--- react-quickstart/src/App.jsx
+++ react-quickstart/src/App.jsx
@@ -1,24 +1,23 @@
-import './App.css'
-import AdminPanel from './AdminPanel';
-import LoginForm from './LoginForm';
+const products = [
+  { title: 'Cabbage', isFruit: false, id: 1 },
+  { title: 'Garlic', isFruit: false, id: 2 },
+  { title: 'Apple', isFruit: true, id: 3 },
+];
 
-function App() {
+export default function ShoppingList() {
+  const listItems = products.map(product =>
+    <li
+      key={product.id}
+      style={{
+        color: product.isFruit ? 'magenta' : 'darkgreen'
+      }}
+    >
+      {product.title}
+    </li>
+  );
 
-  let content;
-  let isLoggedIn = true;
-
-if (isLoggedIn) {
-  content = <AdminPanel />;
-} else {
-  content = <LoginForm />;
+  return (
+    <ul>{listItems}</ul>
+  );
 }
 
-  return(
-    <>
-     <h1>Welcome to my App!</h1>
-     {content}
-    </>
-  )
-}
-
-export default App
