dde2c732e9936bb3409171a4f6d873cccdef8d3f
EMD-001 <elloisadegula.01@gmail.com>
2024-10-13 16:15:29 +0800

    I’m done completing the section on Writing Markup with Jsx

--- react-quickstart/src/App.jsx
+++ react-quickstart/src/App.jsx
@@ -1,14 +1,20 @@
-function MyButton() {
+function AboutPage() {
   return (
-    <button>I'm a Button!</button>
-  )
-}
-function App() {
-  return(
     <>
-     <h1>Welcome to my App!</h1>
-     <MyButton />
+      <h1>About</h1>
+      <p>
+        Hello there. <br /> How do you do?
+      </p>
     </>
-  )
+  );
 }
-export default App
+
+function App() {
+  return (
+    <div>
+      <h1>Welcome to my app</h1>
+      <AboutPage />
+    </div>
+  );
+}
+export default App;
