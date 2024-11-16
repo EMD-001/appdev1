38f91d0c2683511cc3c54f72776f0ea799ffed04
EMD-001 <elloisadegula.01@gmail.com>
2024-11-16 13:41:14 +0800

    Done with Text Input

--- Assignment 8/react-hooks/src/main.jsx
+++ Assignment 8/react-hooks/src/main.jsx
@@ -3,10 +3,12 @@ import { createRoot } from 'react-dom/client'
 import './index.css'
 import Counter from './counter.jsx'
 import ToggleButton from './ToggleButton.jsx'
+import TextInput from './TextInput.jsx'
 
 createRoot(document.getElementById('root')).render(
   <StrictMode>
     <Counter />
     <ToggleButton />
+    <TextInput />
   </StrictMode>,
 )
