d4d04b36485df0377559c84de124a7668a4de1f9
EMD-001 <elloisadegula.01@gmail.com>
2024-11-16 13:50:47 +0800

    Done with Fetch Posts

--- Assignment 8/react-hooks/src/main.jsx
+++ Assignment 8/react-hooks/src/main.jsx
@@ -4,11 +4,12 @@ import './index.css'
 import Counter from './counter.jsx'
 import ToggleButton from './ToggleButton.jsx'
 import TextInput from './TextInput.jsx'
-
+import FetchPosts from './FetchPosts.jsx'
 createRoot(document.getElementById('root')).render(
   <StrictMode>
     <Counter />
     <ToggleButton />
     <TextInput />
+    <FetchPosts />
   </StrictMode>,
 )
